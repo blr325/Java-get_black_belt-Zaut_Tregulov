2d07f72c00eb39b8b7a57b9e3c6cafe2c4fb8580
Andrei <surginevich1998@gmail.com>
2022-03-01 16:38:12 +0300

    practiced coding the PriorityQueue

--- src/main/java/collection/queue/PriorityOueue.java
+++ src/main/java/collection/queue/PriorityOueue.java
@@ -5,5 +5,18 @@ import java.util.PriorityQueue;
 public class PriorityOueue {
     public static void main(String[] args) {
         PriorityQueue<String> priorityQueue = new PriorityQueue<>(32);
+        priorityQueue.offer("Ян");
+        priorityQueue.offer("Андрей");
+        priorityQueue.offer("Михаил");
+        priorityQueue.offer("Сергей");
+        priorityQueue.offer("Федор");
+        priorityQueue.offer("Sam");
+        System.out.println("priorityQueue = " + priorityQueue);
+        System.out.println(priorityQueue.poll());
+        System.out.println(priorityQueue.poll());
+        System.out.println(priorityQueue.poll());
+        System.out.println(priorityQueue.poll());
+        System.out.println(priorityQueue.poll());
+        System.out.println(priorityQueue.poll());
     }
 }
