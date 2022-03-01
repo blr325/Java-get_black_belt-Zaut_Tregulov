5faf4ddcc83ff15830f367ee64c72069e226a938
Andrei <surginevich1998@gmail.com>
2022-03-01 16:29:56 +0300

    practiced coding the LinkedListQueue

---
+++ src/main/java/collection/queue/PriorityOueue.java
@@ -0,0 +1,9 @@
+package collection.queue;
+
+import java.util.PriorityQueue;
+
+public class PriorityOueue {
+    public static void main(String[] args) {
+        PriorityQueue<String> priorityQueue = new PriorityQueue<>(32);
+    }
+}
