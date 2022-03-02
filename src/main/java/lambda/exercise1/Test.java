21d183aa151a04c3bc4d04196981ebce8ec15123
Andrei <surginevich1998@gmail.com>
2022-03-02 13:02:44 +0300

    practiced coding the lambda

--- src/main/java/lambda/exercise1/Test.java
+++ src/main/java/lambda/exercise1/Test.java
@@ -1,6 +1,8 @@
 package lambda.exercise1;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Test {
     public static void printInfo(ArrayList<Student> students, CheckStudent checkStudent) {
@@ -20,8 +22,24 @@ public class Test {
         students.add(new Student("Evgeniy", 'm', 25, 6.3));
         students.add(new Student("Anastasia", 'w', 18, 8.6));
 
-        printInfo(students, (Student st) -> st.getAvrGrade() > 7.0);
-        printInfo(students, (Student st) -> st.getAge() > 20);
-        printInfo(students, (Student st) -> st.getAge() > 20 && st.getAvrGrade() > 7.0 && st.getSex() == 'w');
+        printInfo(students, st -> st.getAvrGrade() > 7.0);
+        printInfo(students, st -> st.getAge() > 20);
+        printInfo(students, st -> st.getAge() > 20 && st.getAvrGrade() > 7.0 && st.getSex() == 'w');
+
+        Comparator<Student> comparatorAge = (st1, st2) -> st1.getAge() - st2.getAge();
+        students.sort(comparatorAge);
+        students.forEach(System.out::println);
+        System.out.println("-------------------------------------------------------");
+        Comparator<Student> comparatorName = (st1,st2)-> st1.getName().compareTo(st2.getName());
+        students.sort(comparatorName);
+        students.forEach(System.out::println);
+        System.out.println("-------------------------------------------------------");
+        students.sort(Comparator.comparingDouble(Student::getAvrGrade));
+        students.forEach(System.out::println);
+
+
+
+
+
     }
 }
