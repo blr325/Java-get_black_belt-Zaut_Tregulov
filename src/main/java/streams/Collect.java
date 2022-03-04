e72abd9e9b0cd387ef331fa2ef0041460e49d642
Andrei <surginevich1998@gmail.com>
2022-03-04 18:24:59 +0300

    practiced coding the stream and its method groupingBy and getAvrGrade

--- src/main/java/streams/Collect.java
+++ src/main/java/streams/Collect.java
@@ -13,7 +13,6 @@ public class Collect {
         students.add(new Student("Nikolay", 'm', 19, 9.2));
         students.add(new Student("Evgeniy", 'm', 21, 6.3));
         students.add(new Student("Anastasia", 'w', 19, 8.6));
-
         Map<Integer, List<Student>> mapAge = students.stream().collect(Collectors.groupingBy(Student::getAge));
         mapAge.forEach((i, l) -> System.out.println(i + ":" + l));
         Map<Boolean, List<Student>> mapAvrGradeOver8 =
